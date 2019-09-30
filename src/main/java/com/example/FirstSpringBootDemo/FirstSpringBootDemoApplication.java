f7f4e01b561c3d7971b505a3cbd442c206f9116f
SHUSHANTDHORE <shushantrao@gmail.com>
2019-09-30 09:24:56 +0530

    first commit

---
+++ src/main/java/com/example/FirstSpringBootDemo/FirstSpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.FirstSpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootDemoApplication.class, args);
+	}
+}
